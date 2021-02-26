3034fbb5f88fc7df4d267b66be2096905d8b95cc
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-02-26 21:01:19 +0300

    Update. Bugs fixed

---
+++ Assets/Drive.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    Rigidbody rb;
+    float z;
+    public float x;
+    public Transform LBwheel;
+    public Transform RBwheel;
+    public Transform LFwheel;
+    public Transform RFwheel;
+    public GameObject VehicleModel;
+    public float RotationLimit; // Лимиты поворота колес. В градусах. Пример : -50, 50
+
+    public bool ReturnWheelRotation = false; // Возвращать ли колеса в исходное положение, когда клавиши поворота отжаты.
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        z = Input.GetAxis("Vertical") * speed;
+        x = Input.GetAxis("Horizontal") * rotationSpeed;
+        bool realistic = ReturnWheelRotation ? x != 0 : true;
+        int axis = z > 0 ? 1 : -1;
+        if (LFwheel.localEulerAngles.y < 358 && z != 0 && realistic) // Влево
+        {
+            print($"Поворачиваем влево.., потому что {LFwheel.rotation.y}, {z}");
+            RaycastHit hit;
+            Physics.Raycast(LBwheel.transform.position, LBwheel.transform.right,
+                out hit, Mathf.Infinity, 1 << 8, QueryTriggerInteraction.Collide);
+            Debug.DrawRay(LBwheel.transform.position, LBwheel.transform.right, Color.red);
+            //print("Точка найдена! Поворот..");
+            if (hit.point != Vector3.zero)
+                transform.RotateAround(hit.point, new Vector3(0, axis, 0), (-speed / 9.25f) * (1 / hit.distance)); //rotationSpeed * (1/hit.distance) * 1.6f
+        }
+        if (RFwheel.localEulerAngles.y > 2 && z != 0 && realistic) // Вправо
+        {
+            print($"Поворачиваем вправо.., потому что {LFwheel.rotation.y}, {z}");
+            RaycastHit hit;
+            Physics.Raycast(RBwheel.transform.position, RBwheel.transform.right,
+                out hit, Mathf.Infinity, 1 << 8, QueryTriggerInteraction.Collide);
+            Debug.DrawRay(RBwheel.transform.position, -RBwheel.transform.right, Color.red);
+            //print("Точка найдена! Поворот..");
+            if (hit.point != Vector3.zero)
+                transform.RotateAround(hit.point, new Vector3(0, -axis, 0), (-speed / 9.25f) * (1 / hit.distance)); //10.75f
+        }
+        /*if (ReturnWheelRotation && !realistic)
+        {
+            LFwheel.GetComponent<rot>().SetDefaultRotation();
+            RFwheel.GetComponent<rot>().SetDefaultRotation();
+        }*/
+
+        /*if (LFwheel.localRotation.y < 0 && x < 0.3 && z > speed / 2)
+        {
+            Drift(Dir.Left);
+        }
+        if (LFwheel.localRotation.y > 0 && x > -0.3 && z > speed / 2)
+        {
+            Drift(Dir.Right);
+        }*/
+    }
+    private void FixedUpdate()
+    {
+        float c = 1;
+        if (z == 0) c = 0.2f;
+        rb.AddForce(VehicleModel.transform.forward * z * c);
+    }
+    enum Dir
+    {
+        Left,
+        Right
+    }
+    /*void Drift(Dir direction)
+    {
+        Vector3 rotation = new Vector3(0, direction == Dir.Left ? -1 : 1, 0);
+        rb.AddTorque(rotation * 800);
+    }*/
+}
---
+++ Assets/rot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rot : MonoBehaviour
+{
+    public float speed;
+    public float y;
+    public float Limit;
+    public Transform car;
+    float yRotation;
+    private void Start()
+    {
+        //MinLimit = 360 + car.GetComponent<Drive>().RotationLimits.x;
+        Limit = car.GetComponent<Drive>().RotationLimit;
+    }
+    float Clamp(float value, float l)
+    {
+        if (180 - value < 0) value = 360 - l;
+        else value = l;
+        print("v = " + value); 
+        return value;
+    }
+    void RotateObject(float y)
+    {
+        transform.Rotate(Vector3.up * y);
+        if (!get(transform.localEulerAngles.y))
+        {
+            float e = transform.localEulerAngles.y;
+            print("e = " + e);
+            float angle = Clamp(e, Limit);
+            Quaternion target = Quaternion.Euler(0, angle, 0);
+            transform.localRotation = target;
+            print(angle);
+        }
+    }
+    bool get(float a)
+    {
+        float limit = Limit;
+        return a > 360 - limit || a < limit; 
+    }
+    void Update()
+    {
+        y = Input.GetAxis("Horizontal") * speed;
+        RotateObject(y);
+    }
+    public void SetDefaultRotation() => transform.localRotation = Quaternion.identity;
+}
